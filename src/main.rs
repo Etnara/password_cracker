fee9b2181df2fb511e1eff9bb55585383666911d
Etnara <zakbutt18@gmail.com>
2022-10-27 23:28:01 -0500

    Start Collecting Arguments
    Collect 2 arguments
    Put them in variables
    Print them

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,13 @@
+use std::env;
+
 fn main() {
-    println!("Hello, world!");
+
+    let args: Vec<String> = env::args().collect();
+
+    // Add check for number and type of arguments
+    // Probably a help page as well
+    let first: &String = &args[1];
+    let second: &String = &args[2];
+
+    println!("First argument: {:?}\nSecond argument: {:?}", first, second);
 }
